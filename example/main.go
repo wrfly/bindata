65ade5a5e766c6f52cd20a4a2318c432d84afcb5
wrfly <mr.wrfly@gmail.com>
2018-12-26 00:40:36 +0800

    chore: usage example

--- example/main.go
+++ example/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/wrfly/bindata/example/asset"
@@ -19,7 +20,25 @@ func init() {
 }
 
 func main() {
-	http.Handle("/", asset.Data)
-	http.Handle("/html", asset.Data)
+	embeddedData := asset.Data
+
+	// list files
+	assets := embeddedData.List()
+	for _, file := range assets {
+		info, _ := file.File()
+		stat, _ := info.Stat()
+		fmt.Printf("contains file: %s [%d]\n",
+			stat.Name(), stat.Size())
+	}
+
+	// open one file
+	n := "/sub/sub_main.css"
+	f, _ := embeddedData.Open(n)
+	bs, _ := ioutil.ReadAll(f)
+	fmt.Printf("file %s's data: %s\n", n, bs)
+
+	// serve http
+	http.Handle("/", embeddedData)
+	http.Handle("/html", embeddedData)
 	http.ListenAndServe(listen, nil)
 }
